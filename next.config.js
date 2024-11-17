a00db93157734a7392a64fd8eeab11b85da629e6
hooyousheng <hushaoxin3000@163.com>
2024-11-18 00:43:24 +0800

    20241118 043

--- next.config.js
+++ next.config.js
@@ -10,6 +10,26 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
+  },
+  experimental: {
+    runtime: 'edge',
+    serverComponents: true,
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
   }
 }
 
