11ffb32d52d4734bf4e448022920a9ccb769da97
hooyousheng <hushaoxin3000@163.com>
2024-11-18 00:47:12 +0800

    20241118 047

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['api.coze.cn', 'www.imagehub.cc', 's.coze.cn', 's1.imagehub.cc'],
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
   }
 }
 
