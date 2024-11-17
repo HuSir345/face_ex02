31d965dff0d561da01db9aaeef0c75e31e2c8120
hooyousheng <hushaoxin3000@163.com>
2024-11-18 00:56:16 +0800

    20241118 056

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.coze.cn'
-      },
-      {
-        protocol: 'https',
-        hostname: '*.imagehub.cc'
-      }
-    ]
+    domains: ['api.coze.cn', 'www.imagehub.cc', 's.coze.cn', 's1.imagehub.cc'],
   },
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**/*': ['./public/**/*']
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/.git/**', '**/node_modules/**']
     }
+    return config
   }
 }
 
