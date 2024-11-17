8af144195b571a1a9b0b18cfe82bc63efc06fb23
hooyousheng <hushaoxin3000@163.com>
2024-11-18 00:48:55 +0800

    20241118 048

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['api.coze.cn', 'www.imagehub.cc', 's.coze.cn', 's1.imagehub.cc'],
+    unoptimized: true
   }
 }
 
